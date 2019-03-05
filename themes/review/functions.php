8a571250811e57f921d72c85dc2d9eede6e643d5
Mauricio Flores <contact@maurojflores.com>
2019-03-05 17:49:53 -0600

    Theme support features setup

--- themes/review/functions.php
+++ themes/review/functions.php
@@ -1,5 +1,58 @@
 <?php
 
+/**
+ * Setup 
+ */
+function rev_setup() {
+    add_theme_support( 'post-thumbnails' );
+
+    // to support all formats, get_post_format_slugs() returns all post formats
+    // otherwise pass array of specific post formats
+    add_theme_support( 'post-formats', get_post_format_slugs() );
+
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+    add_theme_support( 'custom-logo' );
+
+    add_theme_support( 'custom-header-uploads' );
+    
+    add_theme_support( 'custom-header' );
+    
+    add_theme_support( 'custom-background' );
+    
+    add_theme_support( 'title-tag' );
+
+    add_theme_support( 'automatic-feed-links' );
+
+    add_theme_support( 'customize-selective-refresh-widgets' );
+
+    // add_theme_support( 'starter-content' );
+
+    register_nav_menus( array(
+        'main_nav' => 'Main Navigation'
+    ) );
+}
+add_action( 'after_setup_theme', 'rev_setup' );
+
+/** 
+ * Widgets
+ */
+function rev_widget_areas_init() {
+    register_sidebar( array(
+        'name' => 'Main Sidebar',
+        'id' => 'main-sidebar',
+        'description' => 'This is the main sidebar',
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>'
+    ) );
+}
+add_action( 'widgets_init', 'rev_widget_areas_init' );
+
+/**
+ * Scripts and styles
+ */
 function review_scripts() {
     wp_enqueue_style( 'review-style', get_stylesheet_uri(), array(), '0.0.1' );
 }
--- themes/review/index.php
+++ themes/review/index.php
@@ -2,4 +2,34 @@
 
 <h1>Index</h1>
 
+<?php
+if ( get_theme_support('post-thumbnails') ) {
+    echo 'Post thumbnails are supported<br>';
+} else {
+    echo 'Post thumbnails are NOT supported<br>';
+}
+
+$post_formats_slugs = get_post_format_slugs();
+echo 'Post format slugs: ';
+print_r( $post_formats_slugs );
+
+$post_formats_strings = get_post_format_slugs();
+echo 'Post format strings: ';
+print_r( $post_formats_strings );
+
+echo 'Theme starter content: ',
+$theme_support = get_theme_support( 'starter-content' );
+echo $theme_support[0];
+
+
+// function test( $something ) {
+//     echo 'Num args: ' . func_num_args() . '<br>';
+//     echo 'Get args: ' . func_get_args() . '<br>';
+//     $sliced = array_slice( func_get_args(), 1 );
+//     echo 'sliced: ';
+//     print_r( $sliced );
+// }
+// test( 'foo', 'bar', 'baz' );
+?>
+
 <?php get_footer(); ?>

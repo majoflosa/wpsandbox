ab09c70fe3b708192a7f74a53b1c9d7c71e641ab
Mauricio Flores <contact@maurojflores.com>
2019-03-08 10:56:23 -0600

    Initial loop on index.php

--- themes/review/header.php
+++ themes/review/header.php
@@ -7,30 +7,41 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
-    <div id="page" class="page-wrap">
-        
-        <header class="site-header">
-            <div class="site-header-logo">LOGO</div>
-            <?php
-                $nav_args = array(
-                    'menu'          => 178,
-                    'menu_class'    => 'site-nav-links',
-                    'menu_id'       => 'site-nav-links',
-                    'container'     => 'nav',
-                    'container_class' => 'site-nav',
-                    'container_id'  => 'site-nav',
-                    'fallback_cb'   => 'wp_page_menu',
-                    'before'        => '<span class="before-link-markup">',
-                    'after'         => '</span>',
-                    'link_before'   => '',
-                    'link_after'    => '',
-                    'echo'          => true,
-                    'depth'         => 1,
-                    // 'walker'        => ???
-                    'theme_location' => 'main_nav',
-                    'items_wrap'    => '<ul class="%1$s" id="%2$s" data-custom="This is a custom attr">%3$s</ul>',
-                    'item_spacing'  => 'preserve'
-                );
-                wp_nav_menu( $nav_args );
-            ?>
-        </header>
+    <header class="site-header">
+        <h1 class="site-header__title <?php if ( !has_custom_logo() ) echo 'no-logo'; ?>">
+            <span class="site-header__logo-wrap">
+                <?php if ( has_custom_logo() ) the_custom_logo(); ?>
+            </span>
+            <a class="site-header__title-link href="<?php echo esc_url( home_url('/') ); ?>">
+                <?php bloginfo( 'name' ); ?>
+            </a>
+        </h1>
+
+        <?php
+            $nav_args = array(
+                'menu'          => 178,
+                'menu_class'    => 'site-header__nav-links',
+                'menu_id'       => 'site-header__nav-links',
+                'container'     => '',
+                // 'container_class' => 'site-header-nav',
+                // 'container_id'  => 'site-header-nav',
+                'fallback_cb'   => 'wp_page_menu',
+                'before'        => '<span class="before-link-markup">',
+                'after'         => '</span>',
+                'link_before'   => '',
+                'link_after'    => '',
+                'echo'          => true,
+                'depth'         => 1,
+                // 'walker'        => ???
+                'theme_location' => 'main_nav',
+                'items_wrap'    => '<ul class="%1$s" id="%2$s" data-custom="This is a custom attr">%3$s</ul>',
+                'item_spacing'  => 'preserve'
+            );
+        ?>
+        <nav id="site-header__nav" class="site-header__nav">
+            <button class="responsive-nav-toggle">MENU</button>
+            <?php wp_nav_menu( $nav_args ); ?>
+        </nav>
+    </header>
+
+    <div id="page-wrap">
--- themes/review/index.php
+++ themes/review/index.php
@@ -1,9 +1,78 @@
 <?php get_header(); ?>
+<!-- <?php // global $wp_query; ?>
+<pre><?php // print_r( $wp_query ); ?></pre> -->
 
-<h1>Index</h1>
+<div class="template-label"><small>index.php</small></div>
+
+
+<section id="primary" class="primary">
 
 <?php
 
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            /**
+             * $post
+             * 
+             * ID, post_author, post_date, post_date_gmt, post_content, 
+             * post_title, post_excerpt, post_status, comment_status, 
+             * ping_status, post_password, post_name, to_ping, pinged,
+             * post_modified, post_modified_gmt, post_content_filtered,
+             * post_parent, guid, menu_order, post_type, post_mime_type,
+             * comment_count, filter
+             */
+            the_post();
+
+/**
+ * Post template tags
+ * 
+ * body_class(), post_class(), sticky_class()
+ * previous_post_link(), previous_posts_link(), next_post_link(), next_posts_link(),
+ * previous_image_link(), next_image_link(), 
+ * post_password_required(),
+ * posts_nav_link(),
+ * single_post_title(),
+ * the_ID(),
+ * the_title(), get_the_title(), the_title_rss(), the_title_attribute()
+ * the_permalink(), get_permalink(), get_post_permalink(), permalinkk_anchor()
+ * has_post_thumbnail(), the_post_thumbnail(), get_the_post_thumbnail(), get_post_thumbnail_id()
+ * the_content(), the_content_rss(), the_excerpt(), the_excerpt_rss()
+ * the_date(), get_the_date(), the_date_xml(), the_time(), single_month_title()
+ * the_category(), the_category_rss(),
+ * the_meta(), the_tags()
+ * the_modified_date(), the_modified_time(), the_modified_author(), 
+ * the_shortlink(), wp_get_shortlink()
+ * the_attachment_link(), get_attachment_link(), wp_get_attachment_link(), is_attachment()
+ * wp_attachment_is_image(), wp_get_attachment_image(), wp_get_attachment_img_src(), wp_get_attachment_metadata()
+ * the_search_query()
+ * wp_link_pages(), 
+ * edit_post_link(), get_edit_post_link()
+ */
 ?>
+
+    <article <?php post_class( 'entry' ); ?>">
+        <header class="entry__header">
+            <?php the_title( 
+                '<h2 class="entry__title"><a href="' . esc_url( get_the_permalink() ) . '">', 
+                '</a></h2>' 
+            ); ?>
+        </header>
+        <main class="entry__body">
+            <?php the_excerpt(); ?>
+        </main>
+        <footer class="entry__footer">
+            <?php the_time( get_option('date_format') ); ?>
+            <?php edit_post_link(); ?>
+        </footer>
+    </article>
+
+<?php
+        }
+    } else {
+        esc_html_e( 'There are currently no posts.' );
+    }
+?>
+
+</section>
 
 <?php get_footer(); ?>
